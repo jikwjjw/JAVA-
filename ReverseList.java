9ded4b172bdef7eb61126df70ba9ffd8539ed2c3
ji <37184303+jikwjjw@users.noreply.github.com>
2020-07-04 17:14:40 +0800

    链表反转
    链表反转

--- ReverseList.java
+++ ReverseList.java
@@ -15,8 +15,7 @@ public class Solution {
             p = head.next;//当前节点的后一个节点
             head.next = pre;;//当前节点的前一个结点
            pre = head;//pre往右走
-            head = p;//当前节点往右继续走.
-            
+            head = p;//当前节点往右继续走
         }
         return pre;//当循环结束时,pre所指的就是反转链表的头结点
     }
