19e6ed21bb37a2f749d71653c02cfc9759dd2455
ji <37184303+jikwjjw@users.noreply.github.com>
2020-08-17 21:13:12 +0800

    Create GenerateTrees.java

---
+++ GenerateTrees.java
@@ -0,0 +1,50 @@
+package letcode.tree;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 给定一个整数 n，生成所有由 1 ... n 为节点所组成的 二叉搜索树 。
+ */
+public class GenerateTrees {
+    /**
+     * @param n 3
+     * @return [
+     *   [1,null,3,2],
+     *   [3,2,null,1],
+     *   [3,1,null,null,2],
+     *   [2,1,3],
+     *   [1,null,2,null,3]
+     * ]
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/unique-binary-search-trees-ii
+     */
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) {
+            return new LinkedList<TreeNode>();
+        }
+        return generateTrees(1, n);
+    }
+
+    public List<TreeNode> generateTrees(int start, int end) {
+        List<TreeNode> allTrees = new LinkedList<TreeNode>();
+        if (start > end) {
+            allTrees.add(null);
+            return allTrees;
+        }
+        for (int i = start; i <= end; i++) {
+            List<TreeNode> leftTrees = generateTrees(start, i - 1);
+            List<TreeNode> rightTrees = generateTrees(i + 1, end);
+            for (TreeNode left : leftTrees) {
+                for (TreeNode right : rightTrees) {
+                    TreeNode currTree = new TreeNode(i);
+                    currTree.left = left;
+                    currTree.right = right;
+                    allTrees.add(currTree);
+                }
+            }
+        }
+        return allTrees;
+    }
+}
