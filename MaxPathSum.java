510cdec30d7263ca6164f78fea2ab04c20c37a34
ji <37184303+jikwjjw@users.noreply.github.com>
2020-08-19 09:30:32 +0800

    Create MaxPathSum.java

---
+++ MaxPathSum.java
@@ -0,0 +1,58 @@
+package letcode.tree;
+
+/**
+ * 给定一个非空二叉树，返回其最大路径和。
+ *
+ * 本题中，路径被定义为一条从树中任意节点出发，达到任意节点的序列。该路径至少包含一个节点，且不一定经过根节点。
+ *
+ * 输入: [1,2,3]
+ *
+ *        1
+ *       / \
+ *      2   3
+ *
+ * 输出: 6
+ *
+ * 输入: [-10,9,20,null,null,15,7]
+ *
+ *    -10
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ *
+ * 输出: 42
+ */
+public class MaxPathSum {
+    private static int ans = Integer.MIN_VALUE;
+
+    public static int maxPathSum(TreeNode root) {
+        dfs(root);
+        return ans;
+    }
+
+    public static int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = Math.max(dfs(root.left), 0);
+        int right = Math.max(dfs(root.right), 0);
+        ans = Math.max(ans, root.val + left + right);
+        return root.val + Math.max(left, right);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(-10);
+        TreeNode root1 = new TreeNode(9);
+        TreeNode root2 = new TreeNode(20);
+        TreeNode root3 = new TreeNode(15);
+        TreeNode root4 = new TreeNode(7);
+
+        root.left = root1;
+        root.right = root2;
+        root2.left = root3;
+        root2.right = root4;
+
+        System.out.println(maxPathSum(root));
+    }
+}
