6d016a3090245ed502e637b6d8dd88af1cded7e7
ji <37184303+jikwjjw@users.noreply.github.com>
2020-08-19 20:30:36 +0800

    Update Partition.java

--- Partition.java
+++ Partition.java
@@ -5,6 +5,18 @@ import java.util.ArrayList;
 import java.util.Deque;
 import java.util.List;
 
+/**
+ * 给定一个字符串 s，将 s 分割成一些子串，使每个子串都是回文串。
+ * <p>
+ * 返回 s 所有可能的分割方案。
+ *
+ * 输入: "aab"
+ * 输出:
+ * [
+ *   ["aa","b"],
+ *   ["a","a","b"]
+ * ]
+ */
 public class Partition {
     static List<List<String>> res = new ArrayList<>();
 
