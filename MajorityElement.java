86d6f0d0fead9ab4620b3aa59122bc96f0c73b00
ji <37184303+jikwjjw@users.noreply.github.com>
2020-08-20 21:14:59 +0800

    Create MajorityElement.java

---
+++ MajorityElement.java
@@ -0,0 +1,22 @@
+package letcode.String;
+
+/**
+ * 摩尔投票法：从第一个数开始count=1，遇到相同的就加1，遇到不同的就减1，减到0就重新换个数开始计数，总能找到最多的那个
+ * <p>
+ * 多数元素
+ * 给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
+ * 你可以假设数组是非空的，并且给定的数组总是存在多数元素
+ */
+public class MajorityElement {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        Integer candidate = null;
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+        return candidate;
+    }
+}
