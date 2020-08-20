3ae279b29d94bd273cb56add96c2e6ce1ae29ddb
ji <37184303+jikwjjw@users.noreply.github.com>
2020-08-20 15:52:00 +0800

    Create SortList.java

---
+++ SortList.java
@@ -0,0 +1,76 @@
+package letcode;
+
+/**
+ * 排序链表，归并排序
+ */
+public class SortList {
+    public static ListNode sortList(ListNode head) {
+        // 1、递归结束条件
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        // 2、找到链表中间节点并断开链表 & 递归下探
+        ListNode midNode = middleNode(head);
+        ListNode rightHead = midNode.next;
+        midNode.next = null;
+
+        ListNode left = sortList(head);
+        ListNode right = sortList(rightHead);
+
+        // 3、当前层业务操作（合并有序链表）
+        return mergeTwoLists(left, right);
+    }
+
+    //  找到链表中间节点（876. 链表的中间结点）
+    private static ListNode middleNode(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    // 合并两个有序链表（21. 合并两个有序链表）
+    private static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+
+            curr = curr.next;
+        }
+
+        curr.next = l1 != null ? l1 : l2;
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(4);
+        ListNode l2 = new ListNode(2);
+        ListNode l3 = new ListNode(1);
+        ListNode l4 = new ListNode(3);
+        l1.next = l2;
+        l2.next = l3;
+        l3.next = l4;
+        ListNode head = sortList(l1);
+        while (head != null) {
+            System.out.println(head.val);
+            head = head.next;
+        }
+    }
+}
