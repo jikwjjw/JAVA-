44722e310f5d06911be5a1f8abf7a15e16d9606e
ji <37184303+jikwjjw@users.noreply.github.com>
2020-08-20 16:21:09 +0800

    Create QuickSortList.java

---
+++ QuickSortList.java
@@ -0,0 +1,57 @@
+package letcode;
+
+/**
+ * 排序链表，快速排序
+ */
+public class QuickSortList {
+    public static ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        return quickSort(dummy, null);
+    }
+
+    private static ListNode quickSort(ListNode head, ListNode end) {
+        if (head == end || head.next == end || head.next.next == end) return head;
+        // 将小于划分点的值存储在临时链表中
+        ListNode tmpHead = new ListNode(-1);
+        // partition为划分点，p为链表指针，tp为临时链表指针
+        ListNode partition = head.next;
+        ListNode p = partition;
+        ListNode tp = tmpHead;
+        //将小于划分点的结点放到临时链表中
+        while (p.next != end) {
+            if (p.next.val < partition.val) {
+                tp.next = p.next;
+                tp = tp.next;
+                p.next = p.next.next;
+            } else {
+                p = p.next;
+            }
+        }
+        // 合并临时链表和原链表，将原链表接到临时链表后面即可
+        tp.next = head.next;
+        // 将临时链表插回原链表，注意是插回！（不做这一步在对右半部分处理时就断链了）
+        head.next = tmpHead.next;
+        quickSort(head, partition);
+        quickSort(partition, end);
+        return head.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(4);
+        ListNode l2 = new ListNode(2);
+        ListNode l3 = new ListNode(1);
+        ListNode l4 = new ListNode(3);
+        l1.next = l2;
+        l2.next = l3;
+        l3.next = l4;
+        ListNode head = sortList(l1);
+        while (head != null) {
+            System.out.println(head.val);
+            head = head.next;
+        }
+    }
+}
